12098299034e432797024add5796af055bb92fc9
michino999 <Emroang@gmail.com>
2026-02-20 14:15:48 -0500

    Add files via upload

--- tate.py
+++ tate.py
@@ -2,12 +2,15 @@ def sumar (primero,segundo):
    return primero + segundo
 resultado = sumar (2,2)
 print (resultado)
-
 def multiplicar (primero,segundo): 
    return primero * segundo 
-resultado  = multiplicar (7,4)
+resultado  = multiplicar (2,66)
 print (resultado)   
 def dividir (primero,segundo):
   return primero // segundo  
-resultado = dividir (700,2)  
+resultado = dividir (777,2)  
+print (resultado)
+def restar (primero,segundo):
+   return primero - segundo
+resultado = restar (222,123)
 print (resultado)
