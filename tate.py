140ca5bb34696ff2d37ea7196edfcc278a44c90d
michino999 <Emroang@gmail.com>
2026-02-19 20:30:25 -0500

    Add files via upload

--- tate.py
+++ tate.py
@@ -1,5 +1,13 @@
+def sumar (primero,segundo):
+   return primero + segundo
+resultado = sumar (2,2)
+print (resultado)
 
-def sumar  (primero,segundo):
-  return  primero  + segundo
-resultado = sumar  (2,4)
+def multiplicar (primero,segundo): 
+   return primero * segundo 
+resultado  = multiplicar (7,4)
+print (resultado)   
+def dividir (primero,segundo):
+  return primero // segundo  
+resultado = dividir (700,2)  
 print (resultado)
