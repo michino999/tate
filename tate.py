62b760fad8dab6f02cbe442a827003c4cb26baa2
michino999 <Emroang@gmail.com>
2026-02-21 12:42:45 -0500

    Add files via upload

--- tate.py
+++ tate.py
@@ -6,11 +6,19 @@ def multiplicar (primero,segundo):
    return primero * segundo 
 resultado  = multiplicar (2,66)
 print (resultado)   
-def dividir (primero,segundo):
+def dividir_entero (primero,segundo):
   return primero // segundo  
-resultado = dividir (777,2)  
+resultado = dividir_entero (777,2)  
 print (resultado)
 def restar (primero,segundo):
    return primero - segundo
 resultado = restar (222,123)
 print (resultado)
+def dividir_decimal  (primero,segundo):
+      return primero / segundo  
+resultado = dividir_decimal (7.7,2.4)  
+print (resultado)
+import math
+def raiz (numero):
+       return math.sqrt(numero)
+print(raiz(25))
